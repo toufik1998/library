b00e898d1b355eac2a0a6f2c6154e172c8f08958
UnesseAh <youness.ahasla@gmail.com>
2023-09-06 14:34:42 +0100

    establish connection with database

--- repository/DbConnection.java
+++ repository/DbConnection.java
@@ -1,4 +1,21 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
 public class DbConnection {
+    public static void main(String[] args){
+        String url = "jdbc:mysql://localhost:3306/library_management";
+        String username = "root";
+        String password = "";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            connection.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
